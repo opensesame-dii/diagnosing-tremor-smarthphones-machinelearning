708563531a617f2f57a1a48a14c8bdf7ee0c52aa
shioneko <shioneko37@gmail.com>
2024-01-13 22:59:36 +0900

    convert_all_in_dir作った

--- data_conversion.py
+++ data_conversion.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import glob
+import os
+import datetime
+
 
 def convert(input_file,converted_file):
 
@@ -23,10 +27,31 @@ def convert(input_file,converted_file):
     df3.to_csv(converted_file, float_format = "",header = True, index = False, encoding = "utf-8")
 
 
+def convert_all_in_dir(input_dir, output_dir):
+    # output_dirを作る
+    os.mkdir(output_dir)
+    # input_dirの中にあるディレクトリの一覧を取得する
+    files_dir = [
+        f for f in os.listdir(input_dir) if os.path.isdir(os.path.join(input_dir, f))
+    ]
+    print(files_dir)
+    # ディレクトリの各々に対してループ廻す
+    for f in files_dir:
+        # output_dirの中に，ディレクトリを作る
+        new_dir = os.path.join(output_dir,f)
+        os.mkdir(new_dir)
 
+        # {input_dir}/{f} の中のcsvを全て取得する
+        csv_files = glob.glob('*.csv', root_dir = os.path.join(input_dir, f))
+        print(csv_files)
+        # ↑のそれぞれをconvertする
+        for files in csv_files:
+            convert(os.path.join(input_dir,f,files), os.path.join(new_dir, files))
 
+    return
 
+if __name__ == "__main__":
+    input_dir = os.path.join("var", "data-YYYYMMDDhhmm")
+    output_dir = os.path.join("var", f"data-{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}-converted")
 
-input_file = "sample/Tsuboi_Tremor_Sample2"
-converted_file = "sample/Tsuboi_Tremor_Sample2_conv.csv"
-convert(input_file,converted_file)
+    convert_all_in_dir(input_dir, output_dir)
