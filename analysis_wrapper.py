0a0ee1e4e063a06c5221d25cc8bcba8429b4f495
shioneko <shioneko37@gmail.com>
2024-01-26 22:54:19 +0900

    ASI_delta,ASI_IQRどちらも表示するようにした

--- analysis_wrapper.py
+++ analysis_wrapper.py
@@ -10,11 +10,12 @@ from data_conversion import convert_all_in_dir
 def analysis(input_file):
     timeseries_result = timeseries_analysis(input_file, False, False)
     freq_result = freq_analysis(input_file)
-    analyzed_result = [input_file,freq_result[9][0],freq_result[9][1],freq_result[9][2],freq_result[9][3],timeseries_result[3],timeseries_result[7],timeseries_result[9],timeseries_result[10]]
+    analyzed_result = [input_file,freq_result[9][0],freq_result[9][1],freq_result[9][2],freq_result[9][3],timeseries_result[3],timeseries_result[7],timeseries_result[8],timeseries_result[10],timeseries_result[11]]
     return(analyzed_result)
 
 
 def analysis_all_in_dir(input_dir, output_file):
+    
     # input_dirの中にあるディレクトリの一覧を取得する
     files_dir = [
     f for f in os.listdir(input_dir) if os.path.isdir(os.path.join(input_dir, f))
@@ -22,7 +23,7 @@ def analysis_all_in_dir(input_dir, output_file):
     
     with open(output_file, 'w') as v:
         writer = csv.writer(v)
-        writer.writerow(["filename","RPC_x","RPC_y","RPC_z","RPC_u","TSI","ASI","MIPA","SDIPA"])
+        writer.writerow(["filename","RPC_x","RPC_y","RPC_z","RPC_u","TSI","ASI_delta","ASI_IQR","MIPA","SDIPA"])
     # ディレクトリの各々に対してループ廻す
         for f in files_dir:
             # {input_dir}/{f} の中のcsvを全て取得する
